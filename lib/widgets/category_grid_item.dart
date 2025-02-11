3921c7634e1c9c480c5c4f47edff067480d7fb12
Zenn0 <zenno.vaccarezza@gmail.com>
2025-02-11 02:35:32 +0100

    Fisrt Commit

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryGridItem({Key? key, required this.category}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // Handle tap event
+      },
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.7),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.headlineSmall
+        ),
+      ),
+    );
+  }
+}
